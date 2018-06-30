0e05b3e362254141f10d63c27edf171be516bbe6
mtkgeek <uchihasasukefirst@gmail.com>
2018-06-30 10:58:54 +0100

    update2

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 var button = document.getElementsByClassName('button')[0];
 var menu = document.getElementsByClassName('menu')[0];
 
-function stack(event){
+/*function stack(event){
     menu.classList.toggle('nav-stacked');
 }
 
-button.addEventListener("click", stack);
+button.addEventListener("click", stack);*/
